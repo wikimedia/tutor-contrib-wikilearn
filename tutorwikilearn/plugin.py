34db851f8b8f3d78c93fadf01f5d8eea7e630f81
eemaanamir <eemaan.amir@gmail.com>
2025-10-13 16:55:12 +0500

    fix: fixed lint error

--- tutorwikilearn/plugin.py
+++ tutorwikilearn/plugin.py
@@ -241,7 +241,7 @@ def enable() -> None:
 
 
 @MFE_APPS.add()
-def _add_my_mfe(mfes):
+def _add_my_mfe(mfes):  # type: ignore[no-untyped-def]
     mfes["messenger"] = {
         "repository": "https://github.com/eemaanamir/frontend-app-messenger.git",
         "port": 2010,
